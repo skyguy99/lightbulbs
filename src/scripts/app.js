38f3796b36ffacd4473f8175cff617d677a066a4
Skylar Thomas <skylarth@usc.edu>
2020-04-27 16:38:18 -0700

    color and light logic

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,7 +1,6 @@
 import 'styles/index.scss';
 import { radians, map, distance } from './helpers';
 import {data} from './data.js';
-
 import { TweenLite } from 'gsap/TweenMax';
 import InteractiveControls from './vendor/InteractiveControls';
 
@@ -419,7 +418,7 @@ loader.load(
   }  , 0);
 
   setTimeout( function(){
-    localThis.forceDoneLoading(); //end w song
+    //localThis.forceDoneLoading(); //end w song
 
   }  , 13000);
 
@@ -805,65 +804,91 @@ if(string.toLowerCase().includes('screenroom1'))
   return material;
 }
 
+calculateColor(hex, intensity)
+{
+  //Version 4.0 https://jsfiddle.net/PimpTrizkit/a7ac0qvp/
+  //https://stackoverflow.com/questions/5560248/programmatically-lighten-or-darken-a-hex-color-or-rgb-and-blend-colors
+  const pSBC=(p,c0,c1,l)=>{
+  	let r,g,b,P,f,t,h,m=Math.round,a=typeof(c1)=="string";
+  	if(typeof(p)!="number"||p<-1||p>1||typeof(c0)!="string"||(c0[0]!='r'&&c0[0]!='#')||(c1&&!a))return null;
+  	h=c0.length>9,h=a?c1.length>9?true:c1=="c"?!h:false:h,f=pSBC.pSBCr(c0),P=p<0,t=c1&&c1!="c"?pSBC.pSBCr(c1):P?{r:0,g:0,b:0,a:-1}:{r:255,g:255,b:255,a:-1},p=P?p*-1:p,P=1-p;
+  	if(!f||!t)return null;
+  	if(l)r=m(P*f.r+p*t.r),g=m(P*f.g+p*t.g),b=m(P*f.b+p*t.b);
+  	else r=m((P*f.r**2+p*t.r**2)**0.5),g=m((P*f.g**2+p*t.g**2)**0.5),b=m((P*f.b**2+p*t.b**2)**0.5);
+  	a=f.a,t=t.a,f=a>=0||t>=0,a=f?a<0?t:t<0?a:a*P+t*p:0;
+  	if(h)return"rgb"+(f?"a(":"(")+r+","+g+","+b+(f?","+m(a*1000)/1000:"")+")";
+  	else return"#"+(4294967296+r*16777216+g*65536+b*256+(f?m(a*255):0)).toString(16).slice(1,f?undefined:-2)
+  }
+
+  pSBC.pSBCr=(d)=>{
+  	const i=parseInt,m=Math.round;
+  	let n=d.length,x={};
+  	if(n>9){
+  		const [r, g, b, a] = (d = d.split(','));
+  	        n = d.length;
+  		if(n<3||n>4)return null;
+  		x.r=i(r[3]=="a"?r.slice(5):r.slice(4)),x.g=i(g),x.b=i(b),x.a=a?parseFloat(a):-1
+  	}else{
+  		if(n==8||n==6||n<4)return null;
+  		if(n<6)d="#"+d[1]+d[1]+d[2]+d[2]+d[3]+d[3]+(n>4?d[4]+d[4]:"");
+  		d=i(d.slice(1),16);
+  		if(n==9||n==5)x.r=d>>24&255,x.g=d>>16&255,x.b=d>>8&255,x.a=m((d&255)/0.255)/1000;
+  		else x.r=d>>16,x.g=d>>8&255,x.b=d&255,x.a=-1
+  	}return x
+  };
+  return pSBC(intensity, hex);
+}
+
 updateLights()
 {
 
-    //GET DATA FROM HELPER FILE
-    //console.log('DATA: '+data['02-22-20_11:00AM'][0][0]);
-//
-// if(!this.setupLights){ //check so dont repeat
-// var teststr = '';
-//
-//   var localThis = this;
-//
-//   var dictLength = Object.keys(data).length*10;
-//
-  // this.allLights.forEach(function(l) {
-  //     l.aggregateIntensity = 0;
-  //     l.intensity = 0;
-  //
-  //       Object.keys(data).forEach(function(key) {
-  //       //console.log(key, data[key]);
-  //       if(data[key][0].includes(l.name)){
-  //
-  //         l.aggregateIntensity = (((l.aggregateIntensity)*dictLength)+data[key][1])/(dictLength);
-  //       }
-  //   });
-  //   //console.log(l.name+' | '+l.aggregateIntensity*10);
-  // });
-//
-//   //To set individual point:
-//   this.allLights.forEach(function(l) {
-//     if(data[localThis.currentKey][0].includes(l.name)){
-//       l.intensity = data[localThis.currentKey][1];
-//     }
-//
-//     //check
-//     //console.log(l.name+" = "+l.intensity);
-//   });
-//
-//   this.setupLights = true;
-//
-//   var localThis = this;
-//
-//   console.log("intensity "+this.led.intensity);
-//   console.log("aggregate"+this.led.aggregateIntensity);
-// }
-
 var localThis = this;
+//console.log(this.calculateColor('#42c4fd', 0.8));
 
 if(this.lightsAreImmediateSetting)
 {
     $('#center').children().each(function(i, child) {
+
       $(child).removeClass('active');
+      $(child).children().each(function(i, child2) {
+        if(i == 0)
+        {
+          $(child2).addClass('notShowing');
+        } else if (i == 1)
+        {
+          $(child2).addClass('strikethru');
+        }
+      })
 
       //update each light ----------------
-        var intensity = localThis.currentDataPt[1]*1.2;
+        var intensity = (localThis.currentDataPt[1])/10;
+        var color = '#ffffff';
 
-       $(child).find('filter').attr("stdDeviation", "0");
+        if($(child).attr('name') == 'halogen')
+        {
+          color = '#ffcc00';
+        } else if ($(child).attr('name') == 'led') {
+          color = '#ffffff';
+        }
+        else if ($(child).attr('name') == 'sunlight') {
+          color = '#feffd4';
+        }
+        else if ($(child).attr('name') == 'white neon') {
+          color = '#ff3dbe';
+        }
+        else if ($(child).attr('name') == 'bluelight') {
+          color = '#75e6ff';
+        }
+
+        var fillColor = localThis.calculateColor(color, (localThis.currentDataPt[1])/4);
+        color = localThis.calculateColor(color, (localThis.currentDataPt[1])/18);
+
+       //document.getElementsByTagName("feGaussianBlur")[0].setAttribute("stdDeviation", 3);
        var thing =  $(child).find('use')[0];
-       $(thing).css('fill', '#42c4fd');
+       var thing2 = $(child).find('use')[1];
+       $(thing).css('fill', color);
 
+       $(thing2).css('fill', fillColor);
        $(child).find('h5').text(localThis.currentDataPt[1]);
       //--------------------------------------
 
@@ -871,6 +896,16 @@ if(this.lightsAreImmediateSetting)
         if($(child).attr('name') == obj.trim())
         {
           $(child).addClass('active');
+          $(child).children().each(function(i, child2) {
+            if(i == 0)
+            {
+              $(child2).removeClass('notShowing');
+            } else if (i == 1)
+            {
+              $(child2).removeClass('strikethru');
+            }
+          })
+
         }
       })
     })
@@ -1462,6 +1497,10 @@ getRandomFloat(min, max, decimalPlaces) {
   onScroll(event)
   {
 
+//animate scroll
+var multi = event.deltaY >= 0 ? 1 : -1;
+var change = (multi*10).toString+"px";
+    TweenLite.to($('.scrollBar'), 1, {"margin-top":change});
     //updates current immediate
 
     if(this.lightsAreImmediateSetting)
